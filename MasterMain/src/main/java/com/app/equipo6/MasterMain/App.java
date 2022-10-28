fe2f4ddac84a9265d5571bd366323ce78e212a7e
alvaro <bernabeyalvaro@gmail.com>
2022-10-28 10:23:14 +0200

    first commit

---
+++ MasterMain/src/main/java/com/app/equipo6/MasterMain/App.java
@@ -0,0 +1,13 @@
+package com.app.equipo6.MasterMain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
